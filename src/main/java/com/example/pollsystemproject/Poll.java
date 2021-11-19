b628bf1af968b90d02c805319bfb6d2c372f4f09
Jason Zhang <xinyuanzhang29@gmail.com>
2021-11-19 11:07:36 -0500

    PIN generator

--- src/main/java/com/example/pollsystemproject/Poll.java
+++ src/main/java/com/example/pollsystemproject/Poll.java
@@ -1,6 +1,5 @@
 package com.example.pollsystemproject;
 
-
 //import javax.persistence.criteria.CriteriaBuilder;
 import java.io.PrintWriter;
 import java.io.Serializable;
@@ -10,23 +9,21 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 import java.security.SecureRandom;
 
+public class Poll implements Serializable {
 
-
-
-public class Poll  implements Serializable {
     private String id;
     private String title;
     private String question;
     private status poll_status;
     private String[] choice;
-    private Hashtable<String,String> vote;
+    private Hashtable<String, String> vote;
     private LocalDateTime releaseDate;
 
-    public enum status{
-        created,running,released
+    public enum status {
+        created, running, released
     }
 
-    public Poll(){
+    public Poll() {
         vote = new Hashtable<>();
     }
 
@@ -39,19 +36,28 @@ public class Poll  implements Serializable {
         return str.charAt(rand.nextInt(str.length()));
     }
 
+    public String genPIN() {
+        final String numStr = "0123456789";
+        String PIN = "";
+        for (int i = 0; i < 6; i++) {
+            PIN += getRandomChar(numStr);
+        }
+        return PIN;
+    }
+
     public void setId() {
         final String letterStr = "ABCDEFGHJKMNPQRSTVWXYZ";
         final String numStr = "0123456789";
-        String[] choices = {letterStr, numStr};
+        String[] choices = { letterStr, numStr };
         String newId = "";
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             SecureRandom rand = new SecureRandom();
             int strIdx = rand.nextInt(2);
             newId += getRandomChar(choices[strIdx]);
         }
         this.id = newId;
     }
-    
+
     public String getTitle() {
         return title;
     }
@@ -103,112 +109,112 @@ public class Poll  implements Serializable {
     }
 
     public void create_Poll(String title, String question, String[] choice) throws Exception {
-        if(this.getPoll_status()==null){
+        if (this.getPoll_status() == null) {
             this.setTitle(title);
             this.setQuestion(question);
             this.setPoll_status(status.created);
             this.setChoice(choice);
             this.setId();
-        }else {
+        } else {
             throw new Exception("<h3>Error! You already created one poll!</h3>");
         }
 
-
     }
+
     public void update_Poll(String title, String question, String[] choice) throws Exception {
-        if(this.getPoll_status()== Poll.status.created || this.getPoll_status()== Poll.status.running){
+        if (this.getPoll_status() == Poll.status.created || this.getPoll_status() == Poll.status.running) {
             this.setTitle(title);
             this.setQuestion(question);
             this.setChoice(choice);
             this.setPoll_status(status.created);
-        }else {
+        } else {
             throw new Exception("<h3>Error! Your poll status is not created or running!</h3>");
         }
 
     }
 
     public void clear_Poll() throws Exception {
-        if(this.getPoll_status()== Poll.status.released || this.getPoll_status()== Poll.status.running )
-        {
-            this.setVote(new Hashtable<String,String>());
-            if(this.poll_status == status.released){
+        if (this.getPoll_status() == Poll.status.released || this.getPoll_status() == Poll.status.running) {
+            this.setVote(new Hashtable<String, String>());
+            if (this.poll_status == status.released) {
                 this.setPoll_status(status.created);
             }
-        }else {
+        } else {
             throw new Exception("<h3>Error! Your poll status is not released or running!</h3>");
         }
 
     }
 
     public void close_Poll() throws Exception {
-        if(this.getPoll_status()== Poll.status.released)
-        {
+        if (this.getPoll_status() == Poll.status.released) {
             this.setTitle(null);
             this.setQuestion(null);
             this.setChoice(null);
             this.setPoll_status(null);
-        }else {
+        } else {
             throw new Exception("<h3>Error! Your poll status is not released!</h3>");
         }
     }
+
     public void run_Poll() throws Exception {
-        if(this.getPoll_status()== Poll.status.created)
-        {
+        if (this.getPoll_status() == Poll.status.created) {
             this.setPoll_status(status.running);
-        }else {
+        } else {
             throw new Exception("<h3>Error! You can only run a poll when its state is \"created\"</h3>");
         }
 
-
     }
+
     public void release_Poll() throws Exception {
-        if(this.getPoll_status()== Poll.status.running)
-        {
+        if (this.getPoll_status() == Poll.status.running) {
             LocalDateTime ldt = LocalDateTime.now();
             this.setReleaseDate(ldt);
             this.setPoll_status(status.released);
-        }else{
+        } else {
             throw new Exception("<h3>Error! Your poll status is not running!</h3>");
         }
 
     }
+
     public void unrelease_Poll() throws Exception {
-        if(this.getPoll_status()== Poll.status.released)
-        {
+        if (this.getPoll_status() == Poll.status.released) {
             this.setPoll_status(status.running);
-        }else {
+        } else {
             throw new Exception("<h3>Error! Your poll status is not released!</h3>");
         }
 
     }
+
     public void vote(String id, String choice) throws Exception {
-        if(this.getPoll_status()== status.running){
-            Hashtable<String,String> newHash = this.getVote();
+        if (this.getPoll_status() == status.running) {
+            Hashtable<String, String> newHash = this.getVote();
             newHash.put(id, choice);
             this.setVote(newHash);
-        }else {
+        } else {
             throw new Exception("<h3>Error! Your poll status is not running!</h3>");
         }
 
     }
-    public Hashtable<String, Integer> get_Poll_Result(){
+
+    public Hashtable<String, Integer> get_Poll_Result() {
         Hashtable<String, Integer> result = new Hashtable<>();
         String[] choices = this.getChoice();
-        for(int i =0; i<choices.length;i++){
-            result.put(choices[i],0);
+        for (int i = 0; i < choices.length; i++) {
+            result.put(choices[i], 0);
         }
-        Hashtable<String,String> votes = this.getVote();
+        Hashtable<String, String> votes = this.getVote();
         Enumeration<String> keys = votes.keys();
-        while(keys.hasMoreElements()){
+        while (keys.hasMoreElements()) {
             String key = keys.nextElement();
-           if(result.containsKey(votes.get(key))){
-               int count = result.get(votes.get(key));
-               result.put(votes.get(key), count + 1);
-           }
+            if (result.containsKey(votes.get(key))) {
+                int count = result.get(votes.get(key));
+                result.put(votes.get(key), count + 1);
+            }
         }
         return result;
     }
-    public String download_Poll_Details(PrintWriter output, String filename)  {
+
+    public String download_Poll_Details(PrintWriter output, String filename) {
         LocalDateTime date = this.getReleaseDate();
         String question = this.getQuestion();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
@@ -222,7 +228,7 @@ public class Poll  implements Serializable {
         output.write("\n");
         Enumeration<String> keys = votes.keys();
         int count = 0;
-        while(keys.hasMoreElements()){
+        while (keys.hasMoreElements()) {
             String key = keys.nextElement();
             output.write("Choice[" + count + "]: " + key + " has " + votes.get(key) + " votes\n");
             count++;
@@ -232,5 +238,3 @@ public class Poll  implements Serializable {
     }
 
 }
-
-
